6f8f8ce39ac214d864e4c73b8c4d2be2c19b208d
cubes321 <m.killingback@zen.co.uk>
2025-05-05 20:51:57 +0100

    Added comments

--- smartbot.py
+++ smartbot.py
@@ -32,12 +32,13 @@ class dq:
     def __iter__(self):
         return iter(self.d)
 
+# Load the API key from a file
 with open('e:/ai/genai_api_key.txt') as file:
     api_key = file.read().strip()
 client = genai.Client(api_key=api_key)
 
 # IRC Server details
-
+# load server details, nick, channels to join and system prompt from config file
 def load_config(config_file=sys.argv[1]):
     try:
         config = configparser.ConfigParser()
@@ -77,7 +78,10 @@ def load_config(config_file=sys.argv[1]):
         print(f"Error loading configuration: {e}")
         sys.exit(1)
 
+# Load the configuration file
 config = load_config()
+
+# Print the configuration details to the console
 print("=== IRC Bot Configuration ===")
 print(f"Server: {config['server']['server']}")
 print(f"Port: {config['server']['port']}")
@@ -85,26 +89,30 @@ print(f"Nickname: {config['general']['nick']}")
 print(f"Channels: {config['general']['channels']}")
 print(f"SysPrompt: {config['specifics']['sysprompt']}")
 
+# Assign the configuration values to variables
 SERVER = config['server']['server']
 PORT = config['server']['port']
 NICK = config['general']['nick']
 CHANNELS = config['general']['channels']
 SYSPROMPT = config['specifics']['sysprompt']
 
+# Setup the system prompt and instructions
 sys_instruct_init=f"Limit your output to 450 characters. You are {SYSPROMPT}"
 sys_instruct_news=f"You are {SYSPROMPT}.  Limit your output to 2 paragraphs each at most 450 characters."
 sys_instruct_art= f"You are {SYSPROMPT}.  Limit your output to 2 paragraphs each paragraph not more than 450 characters"
 
+# Setup the Google Search tool
 google_search_tool = Tool(google_search=GoogleSearch())
 
 chats = {}
 chatdeque = {}
 
+# this connects to the server and joins the channels
 def on_connect(connection, event):
     for chan in CHANNELS:
         sys_instruct = f"Limit your output to 450 characters. You are {SYSPROMPT}. The request is of the format '[name]: [request]'.  You don't have to use this format in your answers.  You are in an IRC channel called {chan}. Your name is {NICK}"
         chat = client.chats.create(
-                model="gemini-2.0-flash-thinking-exp",
+                model="gemini-2.5-flash-preview-04-17",
                 config=types.GenerateContentConfig(system_instruction=sys_instruct),                
                 )
         chats[chan] = chat
@@ -116,55 +124,69 @@ def on_connect(connection, event):
         print(result)
         connection.privmsg(chan, result)
 
+# this is the main function that connects to the server and starts the bot
 def main():
     reactor = irc.client.Reactor()
     try:
         c = reactor.server().connect(SERVER, PORT, NICK)
-        c.add_global_handler("welcome", on_connect)
-        c.add_global_handler("pubmsg", on_message)
-        c.add_global_handler("action",on_action)
+        c.add_global_handler("welcome", on_connect)         # this is the welcome message handler
+        c.add_global_handler("pubmsg", on_message)          # this is the public message handler
+        c.add_global_handler("action",on_action)            # this is the action handler
         reactor.process_forever()
     except irc.client.ServerConnectionError:
         print("Connection error")
 
+# this is the action handler that handles actions in the channel
 def on_action(connection,event):
+# setup the imput text for the AI
     inputtext = event.arguments[0].strip()
     inputtext2 = event.arguments[0].strip()
     inputtext = "[" + event.source.nick + " " + inputtext + "]"
     chan = event.target
     logging(event, inputtext)
     if inputtext.find(NICK.lower()) != -1:
-        get_ai_answer(inputtext, connection, event)
+        get_ai_answer(inputtext, connection, event)             # this is the AI answer handler
         return
 
+# this is the routine that handles the messages in public channels
 def on_message(connection, event):
+# setup the imput text for the AI
     inputtext = event.arguments[0].strip()
     inputtext2 = event.arguments[0].strip()
     inputtext = event.source.nick + ": " + inputtext
     chan = event.target
     logging(event, inputtext)
+# check if the message has the bot's name in it 
     if event.arguments[0][:len(NICK)].lower().strip() == NICK.lower():
+# goes to the news routine if the message has !news in it
         if event.arguments[0].find("!news") != -1:
             get_ai_news(event, connection)
             return
+# goes to the art routine if the message has !art in it
         if event.arguments[0].find("!art") != -1:
             get_ai_art(event, connection)
             return
+# goes to the youtube routine if the message has !meme in it
         if event.arguments[0].find("!yt") != -1:
             get_yt_vid(event, connection)
             return
+# goes to the youtube anime routine if the message has !animeyt in it
         if event.arguments[0].find("!animeyt") != -1:
             get_yt_animevid(event, connection)
             return
+# goes to the meme routine if the message has !meme in it
         if event.arguments[0].find("!meme") != -1:
             get_ai_meme(event, connection, chan)
             return
+# deals with the message if no other handlers are called
         get_ai_answer(inputtext, connection, event)
         return
+# add the message to the deque for the channel to be used in the random routine
     chatdeque[chan].append(event.source.nick + ": " + inputtext2)
+# see if the bot will make a random response to a message
     random_range = random.uniform(0,40)
     print(f"random range: {random_range}")    
-    if random_range < (4):
+    if random_range < (4):                      # this is the chance of a random response (4 is 10%)
         print("*** Random routine has fired ***")
         inputqueue = "; ".join(list(chatdeque[chan]))
         print(inputqueue)
@@ -177,9 +199,11 @@ def on_message(connection, event):
             connection.privmsg(event.target,"Random routine error!")
             return
 
+# this is the routine that removes line feeds and carriage returns from the text
 def remove_lfcr(text):
     return text.replace("\n"," ").replace("\r"," ")
 
+# this is the routine that gets the AI answer to the question
 def get_ai_answer(inputtext, connection, event):
     try:
         chan = event.target
@@ -196,6 +220,7 @@ def get_ai_answer(inputtext, connection, event):
         print(e.message)
         connection.privmsg(event.target,"Chat routine error!")
         return    
+# splits the response into paragraphs and sends them to the channel with a delay of 1 second between each paragraph and a maximum of 450 characters each to avoid flooding the channel
     para_text = response.text.splitlines()
     nonempty_para_text = [line for line in para_text if line.strip()]
     for paragraph in nonempty_para_text:
@@ -206,6 +231,7 @@ def get_ai_answer(inputtext, connection, event):
         time.sleep(1)
     return
 
+# this is the routine that gets the joining message for the channel
 def connect_msg():
     response = client.models.generate_content(
         model="gemini-2.0-flash",
@@ -214,10 +240,12 @@ def connect_msg():
     )
     return response.text
 
+# this is the routine that logs the messages to the console
 def logging(event, inputtext):
     print(event.target + ":" + event.source.nick + ": " + event.arguments[0])
     print(event.target + ":" + event.source.nick + ": " + inputtext)
 
+# this is the routine that gets the news from the AI
 def get_ai_news(event, connection):
     try:
         response = client.models.generate_content(
@@ -233,6 +261,7 @@ def get_ai_news(event, connection):
         print(e.message)
         connection.privmsg(event.target,"News routine error!")
         return
+# splits the response into paragraphs and sends them to the channel with a delay of 1 second between each paragraph and a maximum of 450 characters each to avoid flooding the channel
     para_text = response.text.splitlines()
     nonempty_para_text = [line for line in para_text if line.strip()]
     for paragraph in nonempty_para_text:
@@ -243,12 +272,13 @@ def get_ai_news(event, connection):
         time.sleep(1)
     return
 
+# this is the routine that gets the AI art from the image URL (jpg only)
 def get_ai_art(event, connection):
     artlen = len(NICK) + 6
     print(artlen)
     image_path = event.arguments[0][artlen:]
     print(image_path)
-    image = requests.get(image_path)
+    image = requests.get(image_path) # this gets the image from the URL
 # version 1 18/03/2025
     try:
       response = client.models.generate_content(
@@ -263,6 +293,7 @@ def get_ai_art(event, connection):
         connection.privmsg(event.target,"Art routine error!")
         return
 # end of new error trapping routine
+# splits the response into paragraphs and sends them to the channel with a delay of 1 second between each paragraph and a maximum of 450 characters each to avoid flooding the channel
     para_text = response.text.splitlines()
     nonempty_para_text = [line for line in para_text if line.strip()]
     for paragraph in nonempty_para_text:
@@ -273,6 +304,7 @@ def get_ai_art(event, connection):
         time.sleep(1)
     return
 
+# this is the routine that gets a repsonse for a youtube video URL  
 def get_yt_vid(event,connection):
     try:
         connection.privmsg(event.target,"<Analysing video please wait>")
@@ -296,6 +328,7 @@ def get_yt_vid(event,connection):
         print(e.message)
         connection.privmsg(event.target,"YT routine error!")
         return
+# splits the response into paragraphs and sends them to the channel with a delay of 1 second between each paragraph and a maximum of 450 characters each to avoid flooding the channel  
     para_text = response.text.splitlines()
     nonempty_para_text = [line for line in para_text if line.strip()]
     for paragraph in nonempty_para_text:
@@ -306,6 +339,7 @@ def get_yt_vid(event,connection):
         time.sleep(1)
     return
 
+# this is the routine that gets a repsonse for a youtube anime video URL
 def get_yt_animevid(event,connection):
     print("=== YT Anime ===")
     try:
@@ -332,6 +366,7 @@ def get_yt_animevid(event,connection):
         print(e.message)
         connection.privmsg(event.target,"YT routine error!")
         return
+# splits the response into paragraphs and sends them to the channel with a delay of 1 second between each paragraph and a maximum of 450 characters each to avoid flooding the channel
     para_text = response.text.splitlines()
     nonempty_para_text = [line for line in para_text if line.strip()]
     for paragraph in nonempty_para_text:
@@ -342,6 +377,7 @@ def get_yt_animevid(event,connection):
         time.sleep(1)
     return
 
+# this is the routine that gets a meme for the conversation
 def get_ai_meme(event,connection, chan):
     try:
         inputqueue = "; ".join(list(chatdeque[chan]))
@@ -358,6 +394,7 @@ def get_ai_meme(event,connection, chan):
         print(e.message)
         connection.privmsg(event.target,"Meme routine error!")
         return
+# splits the response into paragraphs and sends them to the channel with a delay of 1 second between each paragraph and a maximum of 450 characters each to avoid flooding the channel
     para_text = response.text.splitlines()
     nonempty_para_text = [line for line in para_text if line.strip()]
     for paragraph in nonempty_para_text:
